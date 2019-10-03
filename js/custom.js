ef474f41e466dc73a502c587c4736db23e334f37
Dave Baines <bainezydave@gmail.com>
2019-10-03 19:11:16 +1000

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,44 @@
+(function ()
+{
+    document.getElementById("name").innerHTML = data.name;
+    document.getElementById("email").append(data.email);
+    document.getElementById("mobile").append(data.mobile);
+    document.getElementById("linkedin").append("https://www.linkedin.com/in/" + data.linkedin);
+    document.getElementById("summary").innerHTML += `<p>${data.professional_summary}</p>`;
+
+    for (let job in data.work)
+    {
+        const item = data.work[job];
+
+        document.getElementById("experience").innerHTML += `
+            <div>
+                <h2>${item.role}</h2>
+                <i class="far fa-building"></i><h3>${item.company}</h3>
+                <i class="fas fa-map-marker-alt"></i><h3>${item.location}</h3>
+                <i class="far fa-calendar-alt"></i><h3>${item.start_date} - ${item.end_date}</h3>
+                <p>${item.desctiption}</p>
+            </div>`;
+    }
+
+    for (let course in data.education)
+    {
+        const item = data.education[course];
+
+        document.getElementById("education").innerHTML += `
+            <div>
+                <h2>${item.certification}</h2>
+                <i class="far fa-building"></i><h3>${item.institute}</h3>
+                <i class="fas fa-map-marker-alt"></i><h3>${item.location}</h3>
+                <i class="far fa-calendar-alt"></i><h3>${item.start_date}</h3>
+            </div>`;
+    }
+
+    for (let skill in data.skills)
+    {
+        const item = data.skills[skill];
+
+        document.getElementById("skills").innerHTML += `<h3>${item}</h3>`;
+    }
+
+})();
+
---
+++ js/davidbaines.js
@@ -0,0 +1,118 @@
+var data = {
+    "name"                  : "David James Baines",
+    "email"                 : "bainezy94@gmail.com",
+    "mobile"                : "0433 196 954",
+    "linkedin"              : "davidjamesbaines",
+    "github"                : "Bainezy94",
+    "codepen"               : "",
+    "professional_summary"  : "Experienced Digital Content Manager with a demonstrated history of working in the marketing and advertising industry. Skilled in Front End Web Development, Project Management, Content Management & IT Operations with a strong focus in Customer Service & Teamwork. Graduated from General Assembly.",
+    "skills" : [
+        "HTML5", 
+        "CSS3", 
+        "Javascript", 
+        "Jquery",
+        "Bootstrap", 
+        "Bash", 
+        "Photoshop", 
+        "InDesign", 
+        "Illustrator", 
+        "G Suite", 
+        "Automator", 
+        "Mac OS", 
+        "Mac OS Server", 
+        "Windows", 
+        "MS Office", 
+        "Jira", 
+        "Agile", 
+        "Appspace", 
+        "Coates Switchboard", 
+        "GIT"
+    ],
+    "education": [
+        {
+            "certification" : "Diploma of Information Technology",
+            "institute"     : "Coders Academy",
+            "location"      : "North Sydney",
+            "url"           : "",
+            "start_date"    : "2019",
+            "end_date"      : "2019"
+        },
+        {
+            "certification" : "Front End Web Development Certification",
+            "institute"     : "General Assembly",
+            "location"      : "Sydney",
+            "url"           : "",
+            "start_date"    : "2016",
+            "end_date"      : "2016"
+        },
+        {
+            "certification" : "Higher School Certificate",
+            "institute"     : "Camden High School",
+            "location"      : "Camden",
+            "url"           : "",
+            "start_date"    : "2010",
+            "end_date"      : "2011"
+        }
+    ],
+    "certifications": [
+        
+    ],
+    "work": [
+        {
+            "role"          : "Senior Digital Content Manager, Front End Web Developer",
+            "company"       : "GuihenJones & Retail Motion",
+            "location"      : "Crows Nest",
+            "start_date"    : "1/5/2016",
+            "end_date"      : "",
+            "desctiption"   : "Senior Digital Content Manager & Front End Web Developer supporting the McDonald’s account with a strong knowledge of the processes from client to code. Responsible for; <br>- Delivery of content for each campaign window from client brief through to digital implementation. <br>- Communicating with the extended client service teams to ensure accuracy of each campaign window launch. <br>- Meeting strict deadlines & exceeding client expectations of content delivery."
+        },
+        {
+            "role"          : "Digital Support, Junior Web Developer",
+            "company"       : "GuihenJones & Retail Motion",
+            "location"      : "Crows Nest",
+            "start_date"    : "1/5/2016",
+            "end_date"      : "1/9/2018",
+            "desctiption"   : ""
+        },
+        {
+            "role"          : "Digital Support, Quality Assurance Analyst",
+            "company"       : "GuihenJones & Retail Motion",
+            "location"      : "Crows Nest",
+            "start_date"    : "1/1/2015",
+            "end_date"      : "1/5/2016",
+            "desctiption"   : "Starting at GuihenJones in a small Digital team, I took on several roles and duties including: <br>Managing 4 Mac OS Servers, Providing IT support to our Office and Warehouse of 50 staff, Project Management of the Digital Community Notice Board across McDonald's AU & NZ, Coded, scheduled & QA'd content to be distributed to 5000+ devices for McDonald's Australia & New Zealand Digital Menu Boards."
+        },
+        {
+            "role"          : "Assistant Manager",
+            "company"       : "QBD The Bookshop",
+            "location"      : "Macarthur",
+            "start_date"    : "2012",
+            "end_date"      : "2014",
+            "desctiption"   : "After completion of my HSC I was quickly promoted to Assistant Manager of the highest earning store in our company. I assisted the Manager in everyday running of the store while taking over all responsibilites in my managers absence. During my time in this role I had also worked as Acting Manager in surrounding stores in the absence of their Manager."
+        },
+        {
+            "role"          : "Crew Member",
+            "company"       : "KFC",
+            "location"      : "Mt. Annan",
+            "start_date"    : "2008",
+            "end_date"      : "2009",
+            "desctiption"   : "As a Crew Member for KFC I was able to apply my exisitng retail experience to this position while also learning the how to implement my skills with speed and accuracy in this fast paced environment."
+        },
+        {
+            "role"          : "3IC",
+            "company"       : "QBD The Bookshop",
+            "location"      : "Campbelltown",
+            "start_date"    : "2009",
+            "end_date"      : "2012",
+            "desctiption"   : "In this role, I became familiar with all the necessities of working within a retail environment. I was also taught basic managerial tasks to make the most from the team I got to work with."
+        },
+        {
+            "role"          : "Junior Casual",
+            "company"       : "QBD The Bookshop",
+            "location"      : "Campbelltown",
+            "start_date"    : "2007",
+            "end_date"      : "2009",
+            "desctiption"   : ""
+        }
+    ]
+};
